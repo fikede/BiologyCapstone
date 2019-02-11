fab3cfce310e6936b527a0da516c1d9d4980cc65
fikede <faithikede1@gmail.com>
2019-02-10 21:43:38 -0600

    Initialize the ZoomSlider

--- BiologyCapstone/Form1.Designer.cs
+++ BiologyCapstone/Form1.Designer.cs
@@ -28,13 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.LoadImage = new System.Windows.Forms.Button();
+            this.ZoomSlider = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ZoomSlider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(402, 307);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LoadImage
+            // 
+            this.LoadImage.Location = new System.Drawing.Point(552, 236);
+            this.LoadImage.Name = "LoadImage";
+            this.LoadImage.Size = new System.Drawing.Size(99, 39);
+            this.LoadImage.TabIndex = 1;
+            this.LoadImage.Text = "LoadImage";
+            this.LoadImage.UseVisualStyleBackColor = true;
+            this.LoadImage.Click += new System.EventHandler(this.LoadImage_Click);
+            // 
+            // ZoomSlider
+            // 
+            this.ZoomSlider.Location = new System.Drawing.Point(481, 62);
+            this.ZoomSlider.Name = "ZoomSlider";
+            this.ZoomSlider.Size = new System.Drawing.Size(250, 45);
+            this.ZoomSlider.TabIndex = 2;
+            this.ZoomSlider.Scroll += new System.EventHandler(this.ZoomSlider_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ZoomSlider);
+            this.Controls.Add(this.LoadImage);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ZoomSlider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button LoadImage;
+        private System.Windows.Forms.TrackBar ZoomSlider;
     }
 }
 
--- BiologyCapstone/Form1.cs
+++ BiologyCapstone/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,50 @@ namespace BiologyCapstone
 {
     public partial class Form1 : Form
     {
+        private Image originalImage;
         public Form1()
         {
             InitializeComponent();
+
+            ZoomSlider.Minimum = 1;
+            ZoomSlider.Maximum = 10;
+            //ZoomSlider.Value = 
+        }
+
+
+        private void LoadImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog pic = new OpenFileDialog();
+            pic.InitialDirectory = @"C:\Users\FAITH\Pictures\geography";
+            DialogResult picResult = pic.ShowDialog();
+            String picLocation = pic.FileName;
+            pictureBox1.ImageLocation = picLocation;
+            pictureBox1.Load();
+            originalImage = pictureBox1.Image;                                    
+        }
+
+        private void ZoomSlider_Scroll(object sender, EventArgs e)
+        {
+            if(ZoomSlider.Value > 0)
+            {
+                pictureBox1.Image = null;
+                pictureBox1.Image = PictureZoom(originalImage, new Size(ZoomSlider.Value, ZoomSlider.Value));
+                pictureBox1.Load();
+            }            
+        }
+
+        public Image PictureZoom(Image img, Size size)
+        {
+            Bitmap bm = new Bitmap(img, Convert.ToInt32(img.Width * size.Width), 
+                Convert.ToInt32(img.Height * size.Height));
+            Graphics graph = Graphics.FromImage(bm);
+            graph.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            return bm;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
