23f526bb629c51dc361d58b612ec5ec677411fbe
fikede <faithikede1@gmail.com>
2019-02-10 22:09:49 -0600

    Finished Implementing ZoomSlider to enable zoom-in and zoom-out

--- BiologyCapstone/Form1.cs
+++ BiologyCapstone/Form1.cs
@@ -14,13 +14,14 @@ namespace BiologyCapstone
     public partial class Form1 : Form
     {
         private Image originalImage;
+        private int zoomCentre = 8;
         public Form1()
         {
             InitializeComponent();
 
             ZoomSlider.Minimum = 1;
-            ZoomSlider.Maximum = 10;
-            //ZoomSlider.Value = 
+            ZoomSlider.Maximum = 16;
+            ZoomSlider.Value = zoomCentre;
         }
 
 
@@ -28,6 +29,9 @@ namespace BiologyCapstone
         {
             OpenFileDialog pic = new OpenFileDialog();
             pic.InitialDirectory = @"C:\Users\FAITH\Pictures\geography";
+            //Filter to show only Files with Image extensions
+            //pic.Filter = "Image Files|*.png;*.jpg;*.bmp";
+            //pic.FilterIndex = 0;
             DialogResult picResult = pic.ShowDialog();
             String picLocation = pic.FileName;
             pictureBox1.ImageLocation = picLocation;
@@ -40,18 +44,24 @@ namespace BiologyCapstone
             if(ZoomSlider.Value > 0)
             {
                 pictureBox1.Image = null;
-                pictureBox1.Image = PictureZoom(originalImage, new Size(ZoomSlider.Value, ZoomSlider.Value));
-                pictureBox1.Load();
-            }            
+              pictureBox1.Image = PictureZoom_Function(originalImage, new Size(ZoomSlider.Value, ZoomSlider.Value));
+            }  
+            /*Bitmap newImage = new Bitmap(originalImage, (Convert.ToInt32(originalImage.Width * ZoomSlider.Value)
+               /10 ),(Convert.ToInt32(originalImage.Height * ZoomSlider.Value)/10));
+            //New Graphics object
+            Graphics g = Graphics.FromImage(newImage);
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic; //clean up Image
+            pictureBox1.Image = null;
+            pictureBox1.Image = newImage;*/
         }
 
-        public Image PictureZoom(Image img, Size size)
+        public Image PictureZoom_Function(Image img, Size size)
         {
-            Bitmap bm = new Bitmap(img, Convert.ToInt32(img.Width * size.Width), 
-                Convert.ToInt32(img.Height * size.Height));
-            Graphics graph = Graphics.FromImage(bm);
-            graph.InterpolationMode = InterpolationMode.HighQualityBicubic;
-            return bm;
+            Bitmap newImage = new Bitmap(img, Convert.ToInt32((img.Width * size.Width)/10), 
+                Convert.ToInt32((img.Height * size.Height)/10));
+            Graphics graph = Graphics.FromImage(newImage);  //New Graphics object
+            graph.InterpolationMode = InterpolationMode.HighQualityBicubic; //clean up Image
+            return newImage;
         }
 
         private void Form1_Load(object sender, EventArgs e)
