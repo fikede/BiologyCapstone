7358c0abaccdc5a9b6677c9e6c02dd13c044634c
fikede <faithikede1@gmail.com>
2019-02-24 22:58:43 -0600

    Implemented Brightness control with LockBits

--- BiologyCapstone/Form1.Designer.cs
+++ BiologyCapstone/Form1.Designer.cs
@@ -31,21 +31,25 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.LoadImage = new System.Windows.Forms.Button();
             this.ZoomSlider = new System.Windows.Forms.TrackBar();
+            this.brightnessControl = new System.Windows.Forms.TrackBar();
+            this.EditedImage = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ZoomSlider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.brightnessControl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EditedImage)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(402, 307);
+            this.pictureBox1.Size = new System.Drawing.Size(355, 223);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // LoadImage
             // 
-            this.LoadImage.Location = new System.Drawing.Point(552, 236);
+            this.LoadImage.Location = new System.Drawing.Point(554, 304);
             this.LoadImage.Name = "LoadImage";
             this.LoadImage.Size = new System.Drawing.Size(99, 39);
             this.LoadImage.TabIndex = 1;
@@ -55,17 +59,36 @@
             // 
             // ZoomSlider
             // 
-            this.ZoomSlider.Location = new System.Drawing.Point(481, 62);
+            this.ZoomSlider.Location = new System.Drawing.Point(12, 265);
             this.ZoomSlider.Name = "ZoomSlider";
             this.ZoomSlider.Size = new System.Drawing.Size(250, 45);
             this.ZoomSlider.TabIndex = 2;
             this.ZoomSlider.Scroll += new System.EventHandler(this.ZoomSlider_Scroll);
+            // 
+            // brightnessControl
+            // 
+            this.brightnessControl.Location = new System.Drawing.Point(12, 368);
+            this.brightnessControl.Name = "brightnessControl";
+            this.brightnessControl.Size = new System.Drawing.Size(250, 45);
+            this.brightnessControl.TabIndex = 3;
+            this.brightnessControl.Scroll += new System.EventHandler(this.brightnessControl_Scroll);
+            // 
+            // EditedImage
+            // 
+            this.EditedImage.Location = new System.Drawing.Point(420, 12);
+            this.EditedImage.Name = "EditedImage";
+            this.EditedImage.Size = new System.Drawing.Size(351, 223);
+            this.EditedImage.TabIndex = 4;
+            this.EditedImage.TabStop = false;
+            this.EditedImage.Click += new System.EventHandler(this.EditedImage_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.EditedImage);
+            this.Controls.Add(this.brightnessControl);
             this.Controls.Add(this.ZoomSlider);
             this.Controls.Add(this.LoadImage);
             this.Controls.Add(this.pictureBox1);
@@ -74,6 +97,8 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ZoomSlider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.brightnessControl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EditedImage)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -84,6 +109,8 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button LoadImage;
         private System.Windows.Forms.TrackBar ZoomSlider;
+        private System.Windows.Forms.TrackBar brightnessControl;
+        private System.Windows.Forms.PictureBox EditedImage;
     }
 }
 
--- BiologyCapstone/Form1.cs
+++ BiologyCapstone/Form1.cs
@@ -3,25 +3,29 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+// Reference - StackOverflow
 namespace BiologyCapstone
 {
     public partial class Form1 : Form
     {
         private Image originalImage;
-        private int zoomCentre = 8;
+        private int zoomCentre = 5;
         public Form1()
         {
             InitializeComponent();
 
             ZoomSlider.Minimum = 1;
-            ZoomSlider.Maximum = 16;
+            ZoomSlider.Maximum = zoomCentre * 2;
             ZoomSlider.Value = zoomCentre;
+            brightnessControl.Minimum = 0;
+            brightnessControl.Maximum = 10;
+            //brightnessControl.Value = 0;
         }
 
 
@@ -36,23 +40,17 @@ namespace BiologyCapstone
             String picLocation = pic.FileName;
             pictureBox1.ImageLocation = picLocation;
             pictureBox1.Load();
-            originalImage = pictureBox1.Image;                                    
+            originalImage = pictureBox1.Image;
+            EditedImage.Image = pictureBox1.Image;
         }
 
         private void ZoomSlider_Scroll(object sender, EventArgs e)
         {
             if(ZoomSlider.Value > 0)
             {
-                pictureBox1.Image = null;
-              pictureBox1.Image = PictureZoom_Function(originalImage, new Size(ZoomSlider.Value, ZoomSlider.Value));
-            }  
-            /*Bitmap newImage = new Bitmap(originalImage, (Convert.ToInt32(originalImage.Width * ZoomSlider.Value)
-               /10 ),(Convert.ToInt32(originalImage.Height * ZoomSlider.Value)/10));
-            //New Graphics object
-            Graphics g = Graphics.FromImage(newImage);
-            g.InterpolationMode = InterpolationMode.HighQualityBicubic; //clean up Image
-            pictureBox1.Image = null;
-            pictureBox1.Image = newImage;*/
+              EditedImage.Image = null;
+              EditedImage.Image = PictureZoom_Function(originalImage, new Size(ZoomSlider.Value, ZoomSlider.Value));
+            }
         }
 
         public Image PictureZoom_Function(Image img, Size size)
@@ -68,5 +66,92 @@ namespace BiologyCapstone
         {
 
         }
+        //Still need to figure out how to reset trackBar when program is till runnin but a new image is loaded
+        //Also need to figure out how to undo the change in brightness, when the trackBar is moved backwards
+        private void brightnessControl_Scroll(object sender, EventArgs e)
+        {
+            int currentTrackBarPosition = brightnessControl.Value;
+            double scaledPostion = currentTrackBarPosition * 51.2;
+            if(EditedImage.Image != null)
+            {
+                originalImage = EditedImage.Image;
+                Bitmap bmpOriginalImage = new Bitmap(originalImage);
+                //EditedImage.Image = AdjustPixelDisplayByBrightness_Function(originalImage, scaledPostion);
+                EditedImage.Image = UsingLockBitsToEditPixels(bmpOriginalImage, scaledPostion);
+            }            
+        }
+        public Image AdjustPixelDisplayByBrightness_Function(Image image, double value)
+        {
+            Bitmap bmp = new Bitmap(image);
+            for(int i = 0; i < bmp.Width; i ++)
+            {                
+                for(int j = 0; j < bmp.Height; j ++)
+                {
+                    Color c = bmp.GetPixel(i, j);
+                    int red = c.R;
+                    int green = c.G;
+                    int blue = c.B;
+                    double distance = Math.Sqrt((red * red) + (green * green) + (blue * blue));
+                    //float pixelBrightness = c.GetBrightness();                    
+                    if (distance < value)
+                    {
+                        c = Color.Black;                                               
+                    }
+                    bmp.SetPixel(i, j, c);
+                }
+            }
+            return bmp;
+        }
+
+        public Bitmap UsingLockBitsToEditPixels(Bitmap img, double value)
+        {
+            Bitmap bmp = img;
+            //to use LockBits had to allow unsafe code - Should I still do this???
+            //use LockBit to access pixels with pointers
+            unsafe
+            {
+                BitmapData imageBmpData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height),
+                    ImageLockMode.ReadWrite, bmp.PixelFormat);
+
+                //number of bytes per pixel, height of pixels, width of pixels in bytes
+                int numBytesPerPixel = System.Drawing.Bitmap.GetPixelFormatSize(bmp.PixelFormat) / 8;
+                int heightOfPixels = imageBmpData.Height;
+                int widthOfPixelsinBytes = imageBmpData.Width * numBytesPerPixel;
+
+                //pointer to adress of first pixel - Scan0 gets/sets address of first pixel in bitmap
+                byte* pointerFirstPixel = (byte*)imageBmpData.Scan0;
+
+                //Get the RGB components, check brightness and adjust as necessary
+                for(int i = 0; i < heightOfPixels; i ++)
+                {
+                    //.Stride gets/sets scan width of the bitmap object
+                    //use .Stride to go through each line of the image
+                    byte* currentLine = pointerFirstPixel + (i * imageBmpData.Stride);
+                    for(int j = 0; j < widthOfPixelsinBytes; j = j + numBytesPerPixel)
+                    {
+                        //Get RGB components
+                        int red = currentLine[j];
+                        int green = currentLine[j + 1];
+                        int blue = currentLine[j + 2];
+
+                        double distance = Math.Sqrt((red * red) + (green * green) + (blue * blue));
+                        if (distance < value)
+                        {
+                            currentLine[j] = 0;
+                            currentLine[j + 1] = 0;
+                            currentLine[j + 2] = 0;
+                        }
+                    }
+                }
+                bmp.UnlockBits(imageBmpData);
+            }            
+            return bmp;
+        }
+
+        private void EditedImage_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
+
